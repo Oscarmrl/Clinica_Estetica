c671528625c9867b14c9802ef2aac5a61ce4f216
Oscarmrl <oscaralexandermurillo500@gmail.com>
2025-07-26 09:01:46 -0600

    Implementacion de la seccion de Otras opciones por medio de useParams y use-Fetch

---
+++ src/components/Tratamientos/Otrasopciones.tsx
@@ -0,0 +1,49 @@
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import type { Tratamiento } from "../../types";
+
+export default function Otrasopciones() {
+  const { id } = useParams();
+  const servicio = useFetch<Tratamiento>({
+    url: "http://localhost/crud-php-citas/obtener_servicios.php",
+  });
+
+  const servicioEncontrado = servicio.find((p) => p.id.toString() === id);
+
+  if (!servicioEncontrado || !servicio) return null;
+
+  return (
+    <div className="m-5 md:m-10">
+      <h3 className="md:text-3xl text-xl text-accent font-bold mb-4 text-center">
+        También te podría interesar:
+      </h3>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        {servicio
+          .filter((p) => p.id !== servicioEncontrado.id)
+          .slice(0, 3)
+          .map((servicio) => (
+            <div
+              key={servicio.id}
+              className="rounded-badge bg-base-100 rounded-3xl p-4 hover:shadow-2xl transition-shadow duration-300 border border-blue-50"
+            >
+              <img
+                src="/Clinica_Estetica/home.jpg"
+                alt={servicio.nombre}
+                className="w-full h-40 object-cover rounded"
+              />
+              <h4 className="text-lg text-accent font-semibold mt-2">
+                {servicio.nombre}
+              </h4>
+              <p className="text-lg">{servicio.descripcion.slice(0, 60)}...</p>
+              <span className="font-bold block mt-1">{servicio.precio}</span>
+              <div className="flex gap-4 mt-2 ">
+                <button className="btn btn-outline btn-sm btn-accent">
+                  Leer más
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
