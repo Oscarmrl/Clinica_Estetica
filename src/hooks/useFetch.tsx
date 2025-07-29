35519ebdc6e0921fb8673d0dc0d503632896bf95
Oscarmrl <oscaralexandermurillo500@gmail.com>
2025-07-28 23:13:15 -0600

    feat(useFetch):evita llamada si no hay URL válida

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -5,10 +5,16 @@ export default function useFetch<T>({ url }: endpoit) {
   const [data, setData] = useState<T[]>([]);
 
   useEffect(() => {
+    if (!url) return; // 🔒 evita llamada si no hay URL válida
     fetch(url)
       .then((response) => response.json())
-      .then((data) => setData(data))
-
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setData(data);
+        } else {
+          setData([]);
+        }
+      })
       .catch((error) => console.log("Error al obtener tratamientos:", error));
   }, [url]);
 
