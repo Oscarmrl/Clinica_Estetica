9d8da76b260cf4dee15e2e2c3d585bd9d1d849ec
Oscarmrl <oscaralexandermurillo500@gmail.com>
2025-07-28 23:27:01 -0600

    feat(index.ts):Añadiendo los types de Formulario,Horario,Especialista,Usuario

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,15 +4,53 @@ export type Tratamiento = {
   descripcion: string;
   duracion: number;
   precio: number;
+  foto_servicio: string;
 };
 export type endpoit = {
   url: string;
 };
 
 export type Cita = {
-  id: number;
+  id?: number;
   fecha: string;
   hora: string;
   estado: string;
   notas: string;
+};
+export type Formulariocita = Cita & {
+  paciente_id: string;
+  especialista_id: string;
+  servicio_id: string;
+};
+
+export type RolUsuario = "paciente" | "admin" | "Especialista" | "Empleado";
+
+export type Usuario = {
+  id: number;
+  nombre: string;
+  apellido: string;
+  correo: string | null;
+  telefono: string | null;
+  fechaNa: string; // formato YYYY-MM-DD
+  contraseña: string | null;
+  rol: RolUsuario;
+};
+
+export type Especialista = {
+  especialista_id: number;
+  usuario_id: number;
+  especialidad: string;
+  descripcion: string;
+  foto_url: string;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  telefono: string;
+};
+export type Horario = {
+  id: number;
+  especialista_id: number;
+  dia_semana: string;
+  hora_inicio: string;
+  hora_fin: string;
 };
